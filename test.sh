c8339cd339b0b19c3dac3a7702d291eb8755724b
palani <palani160385@gmail.com>
2022-08-18 08:46:24 +0530

    commit

--- test.sh
+++ test.sh
@@ -4,4 +4,10 @@ STATUS=$(git status | tail -1)
 if [ "$STATUS" = "nothing to commit, working tree clean" ]
 then
         echo "pusing"
+		echo "pused"
+		
+elif  [ "$STATUS" = "nothing added to commit but untracked files present (use \"git add\" to track)" ]
+then
+		echo "nothing to commit, working tree clean"
 fi
+
