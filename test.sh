76cfe5b7e4fd1439859a52aadf144e9987799629
palani <palani160385@gmail.com>
2022-08-18 09:01:30 +0530

    commit

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ if [ "$STATUS" = "nothing to commit, working tree clean" ]
 then
         echo "pusing"
 		echo "pused"
-
-elif  [ "$STATUS" = "no changes added to commit use git add and/or git commit -a" ]
+		
+elif  [ "$STATUS" = "no changes added to commit (use "git add" and/or "git commit -a")" ]
 then
 		echo "nothing to commit, working tree clean"
 		
