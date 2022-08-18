14a0aec7c929e2da851ee609aaa1e9e55aa329ce
palani <palani160385@gmail.com>
2022-08-18 09:54:01 +0530

    test

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 if [[ `git status --porcelain` ]]; then
 echo "no changes added to commit (use "git add" and/or "git commit -a")"
 else
-  echo ""
+  echo "Your branch is up to date with origin, nothing to commit, working tree clean"
 fi
