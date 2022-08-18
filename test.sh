33bfb383c2cabbe0e5200b31d2719ddc48765e3c
palani <palani160385@gmail.com>
2022-08-18 09:52:30 +0530

    test

--- test.sh
+++ test.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-STATUS=$(git status | tail -1)
-if [ "$STATUS" = "nothing to commit, working tree clean" ]
-then
-        echo "pusing"
-                echo "pused"
-        elif [ "$STATUS" = "no changes added to commit (use \"git add and/or git commit -a\")" ]; then
-                echo "nothing to commit, working tree clean"
-
+if [[ `git status --porcelain` ]]; then
+echo "no changes added to commit (use "git add" and/or "git commit -a")"
+else
+  echo ""
 fi
-
